40d1c138bf54b3a37fd7493ec64b2e212fecc534
Kelliton <programmerKelliton@gmail.com>
2022-07-23 17:01:15 -0300

    Adicionando cards

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,7 +2,11 @@ import "./App.css";
 import Card from "../components/card";
 
 function App() {
-    return <div className="App">{/* <Card></Card> */}</div>;
+    return (
+        <div className="App">
+            <Card></Card>
+        </div>
+    );
 }
 
 export default App;
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -11,15 +11,24 @@ export default function Card() {
             />
             <div className="Card__info">
                 <span className="info__type">Perfume</span>
-                <h1 className="info__name"></h1>
-                <p className="info__desc"></p>
+                <h1 className="info__name">Gabrielle Essence Eau De Parfum</h1>
+                <p className="info__desc">
+                    Mussum Ipsum, cacilds vidis litro abertis. Posuere libero
+                    varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit
+                    amet nisi.Aenean aliquam molestie leo, vitae iaculis
+                    nisl.Mais vale um bebadis conhecidiss, que um alcoolatra
+                    anonimis.A ordem dos tratores não altera o pão duris.
+                </p>
 
                 <div className="info__price">
-                    <h1 className="price__current"></h1>
-                    <h2 className="price__preview"></h2>
+                    <h1 className="price__current">$149.99</h1>
+                    <h2 className="price__preview">169.99</h2>
                 </div>
 
-                <button className="info__add"></button>
+                <button className="info__add">
+                    <img src="" alt="" />
+                    <span>Add to Cart</span>
+                </button>
             </div>
         </div>
     );
